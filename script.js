aa36fb7a543cae4f7415da667d393a68f1a06424
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-08-26 23:31:30 +0200

    18.4_v1

--- script.js
+++ script.js
@@ -19,25 +19,81 @@ var movies = [
   }
 ];
 
-var moviesElements = movies.map(function(movie) {
-  return (
-    React.createElement('li', {key: movie.id},
-      React.createElement('h2', {}, movie.title),
-      React.createElement('p', {}, movie.desc),
-      React.createElement('img', {
-        className: 'image',
-        src: movie.image,
-        alt: movie.title,
-        width: '300px',
-        height: '445px'
-      })
-    )
-  );
+var Movie = React.createClass({
+
+  propTypes: {
+    movie: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    var movie = this.props.movie;
+
+    return React.createElement('li', {},
+      React.createElement(MovieTitle, {title: movie.title}, ),
+      React.createElement(MovieDescription, {desc: movie.desc}, ),
+      React.createElement(MovieImage, {src: movie.image, alt: movie.title}, )
+    );
+  }
 });
 
-var element = React.createElement('div', {},
-  React.createElement('h1', {}, 'Lista filmów'),
-  React.createElement('ul', {}, moviesElements)
-);
+var MovieTitle = React.createClass({
+
+  propTypes: {
+    title: React.PropTypes.string,
+  },
+
+  render: function() {
+    return React.createElement('h2', {}, this.props.title)
+  },
+});
+
+var MovieDescription = React.createClass({
+
+  propTypes: {
+    desc: React.PropTypes.string,
+  },
+
+  render: function() {
+    return React.createElement('p', {}, this.props.desc)
+  },
+});
+
+var MovieImage = React.createClass({
+
+  propTypes: {
+    src: React.PropTypes.string,
+    alt: React.PropTypes.string
+  },
+
+  render: function() {
+    return React.createElement('img', {
+      className: 'image',
+      src: this.props.src,
+      alt: this.props.alt,
+      width: '300px',
+      height: '445px'
+    })
+  },
+});
+
+var MovieList = React.createClass({
+
+  render: function() {
+    var moviesCell = movies.map(function(movie) {
+      return React.createElement(Movie, {
+        movie: movie,
+        key: movie.id
+      });
+    });
+    return (
+      React.createElement('div', {},
+        React.createElement('h1', {}, 'Lista filmów'),
+        React.createElement('ul', {}, moviesCell)
+      )
+    )
+  },
+});
+
+var element = React.createElement(MovieList);
 
 ReactDOM.render(element, document.getElementById('app'));
